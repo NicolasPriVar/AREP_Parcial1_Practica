9fa762b5162490c7f425cd6bb26c89885e8cb33c
NicolasPriVar <nicolasprietovargas@gmail.com>
2025-09-09 11:22:16 -0500

    mejora

--- src/main/java/org/example/HttpServer.java
+++ src/main/java/org/example/HttpServer.java
@@ -35,14 +35,59 @@ public class HttpServer {
                 + "\r\n"
                 + "<!DOCTYPE html>\n"
                 + "<html>\n"
+
                 + "<head>\n"
+                + "<title>Parcial 1er corte</title>\n"
                 + "<meta charset=\"UTF-8\">\n"
-                + "<title>Parcial 1</title>\n"
-                + "</head>\n"
-                + "<body>\n"
-                + "<h1>Primer parcial AREP</h1>\n"
-                + "<h4>Servidor con conexión HTTP</h4>\n"
+                + "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"\n>"
+                + "</head>"
+
+                + "<body>"
+                + "<h1>Formulario SET</h1>\n"
+                + "<form action=\"/setkv?key={key}&value={value}\">\n"
+                + "<label for=\"Key\">Key:</label><br>\n"
+                + "<input type=\"text\" id=\"key\" name=\"key\" value=\"\"><br><br>\n"
+                + "<label for=\"Value\">Value:</label><br>\n"
+                + "<input type=\"text\" id=\"value\" name=\"value\" value=\"\"><br><br>\n"
+
+                + "<input type=\"button\" value=\"Crear o cambiar\" onclick=\"loadSetMsg()\">\n"
+                + "</form>\n"
+                + "<h1>Formulario GET</h1>\n"
+                + "<form action=\"/getkv?key={key}\">\n"
+                + "<label for=\"Key\">Key:</label><br\n>"
+                + "<input type=\"text\" id=\"key2\" name=\"key\" value=\"\"><br><br>\n"
+                + "<input type=\"button\" value=\"Pedir\" onclick=\"loadGetMsg()\">\n"
+                + "</form>\n"
+                + "<div id=\"getrespmsg\"></div>\n"
+
+                + "<script>\n" +
+                "    function loadGetMsg() {\n" +
+                "        let keyy = document.getElementById(\"key2\").value;\n" +
+                "        const xhttp = new XMLHttpRequest();\n" +
+                "        xhttp.onload = function () {\n" +
+                "            document.getElementById(\"getrespmsg\").innerHTML =\n" +
+                "                this.responseText;\n" +
+                "        }\n" +
+                "        xhttp.open(\"GET\", \"/getkv?key=\" + keyy);\n" +
+                "        xhttp.send();\n" +
+                "    }\n" +
+                "\n" +
+                "    function loadSetMsg() {\n" +
+                "        let keyyy = document.setElementById(\"key\").value;\n" +
+                "        let keyyyy = document.setElementById(\"value\").value;\n" +
+                "        const xhttp = new XMLHttpRequest();\n" +
+                "        xhttp.onload = function () {\n" +
+                "            document.setElementById(\"setrespmsg\").innerHTML =\n" +
+                "                this.responseText;\n" +
+                "        }\n" +
+                "        xhttp.open(\"SET\", \"/setkv?key=\" + keyyy + \"&value=\" + keyyyy);\n" +
+                "        xhttp.send();\n" +
+                "    }\n" +
+                "</script>\n"
+                + "<h4>Autor: Nicolás Prieto Vargas</h4>\n"
+
                 + "</body>\n"
+
                 + "</html>\n";
 
         out.println(outputLine);
