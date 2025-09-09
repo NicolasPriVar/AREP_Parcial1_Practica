c8b09a467f432159d522aa2fa13203956220ef73
NicolasPriVar <nicolasprietovargas@gmail.com>
2025-09-09 10:51:06 -0500

    Progreso

---
+++ src/main/java/org/example/HttpServer.java
@@ -0,0 +1,54 @@
+package org.example;
+
+import java.net.*;
+import java.io.*;
+
+public class HttpServer {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(36000);
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: 36000.");
+            System.exit(1);
+        }
+
+        Socket clientSocket = null;
+        try {
+            System.out.println("Listo para recibir ...");
+            clientSocket = serverSocket.accept();
+        } catch (IOException e) {
+            System.err.println("Accept failed.");
+            System.exit(1);
+        }
+        PrintWriter out = new PrintWriter(
+                clientSocket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(clientSocket.getInputStream()));
+        String inputLine, outputLine;
+        while ((inputLine = in.readLine()) != null) {
+            System.out.println("Recibí: " + inputLine);
+            if (!in.ready()) {break; }
+        }
+        outputLine = "HTTP/1.1 200 OK\r\n"
+                + "Content-Type: text/html\r\n"
+                + "\r\n"
+                + "<!DOCTYPE html>\n"
+                + "<html>\n"
+                + "<head>\n"
+                + "<meta charset=\"UTF-8\">\n"
+                + "<title>Parcial 1</title>\n"
+                + "</head>\n"
+                + "<body>\n"
+                + "<h1>Primer parcial AREP</h1>\n"
+                + "<h4>Servidor con conexión HTTP</h4>\n"
+                + "</body>\n"
+                + "</html>\n";
+
+        out.println(outputLine);
+        out.close();
+        in.close();
+        clientSocket.close();
+        serverSocket.close();
+    }
+}
